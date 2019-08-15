28ceb707638aff2dce33910a621e73686f847b5a
Sam Khan <sameed.hossain.khan@gmail.com>
2019-08-15 16:50:00 +1000

    added http server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express');
+const http = require('http');
 
 // Import type definitions
 const { typeDefs } = require('./app/graphql/typeDefs')
@@ -10,13 +11,18 @@ const { resolvers } = require('./app/graphql/resolvers')
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers })
+const apollo = new ApolloServer({ typeDefs, resolvers })
 
 const app = express()
-server.applyMiddleware({ app })
+// set up http server with the express app
+const httpServer = http.Server(app);
+// Set the Port
+const PORT = process.env.PORT || 4000;
+
+apollo.applyMiddleware({ app })
 app.use(express.static("client/dist"));
 
 // This `listen` method launches a web-server.  Existing apps
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+httpServer.listen({ port: PORT }, () =>
+  console.log(`🚀 Server listening at port: ${PORT}`)
 )
