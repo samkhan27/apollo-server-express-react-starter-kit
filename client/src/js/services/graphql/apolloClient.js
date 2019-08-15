351329297dfba69701ff9ecf6dd1c7ad5794d09a
Sam Khan <sameed.hossain.khan@gmail.com>
2019-08-15 22:28:46 +1000

    corrected app name and new build

--- client/src/js/services/graphql/apolloClient.js
+++ client/src/js/services/graphql/apolloClient.js
@@ -3,7 +3,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'node-fetch'
 
 const env = process.env.NODE_ENV || 'dev';
-const graphqlURI = env === 'production' ? 'https://assessment-portal-development.herokuapp.com/graphql' : 'http://localhost:4000/graphql'
+const graphqlURI = env === 'production' ? 'https://apollo-express-react-starter-s.herokuapp.com/graphql' : 'http://localhost:4000/graphql'
 const client = new ApolloClient({
   uri: graphqlURI,
   fetch: fetch
